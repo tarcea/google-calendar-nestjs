efc19cec81884e8d93dca36b294b673220bbd0e2
tarcea <tarcea@gmail.com>
2023-03-29 21:47:44 +0200

    added nodemailer and ics

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class MailerService {}
+export class MailerService {
+  constructor(private config: ConfigService) {}
+}
