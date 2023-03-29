ecef515114504793935bb38f4302c2ede7b890cf
tarcea <tarcea@gmail.com>
2023-03-29 21:58:19 +0200

    sendNewEmail implemented

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,7 +1,54 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import nodemailer, { Transporter } from 'nodemailer';
 
 @Injectable()
 export class MailerService {
   constructor(private config: ConfigService) {}
+
+  private readonly hostname = this.config.get('SMTP_HOST');
+  private readonly username = this.config.get('SMTP_USERNAME');
+  private readonly password = this.config.get('SMTP_PASSWORD');
+  private readonly port = +this.config.get('SMTP_PORT');
+  private readonly sender = this.config.get('SMTP_SENDER');
+
+  private readonly transporter: Transporter = nodemailer.createTransport({
+    host: this.hostname,
+    port: this.port,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: this.username,
+      pass: this.password,
+    },
+    // logger: true,
+  });
+
+  async sendNewEmail(sendTo: string) {
+    try {
+      await this.transporter.sendMail({
+        from: this.sender,
+        to: sendTo,
+        subject: 'You have a new booking',
+        text: '',
+        html: `
+					<h3>
+						Hi, your booking is confirmed!!
+					</h3>
+					<p>This is just a test to see how the attached calendar event looks like in different email clients.</p>
+				`,
+        // icalEvent: {
+        //   // filename: 'invitation.ics',
+        //   method: 'request',
+        //   content: ics,
+        // },
+        attachments: [
+          {
+            filename: 'booking.ics',
+            content: 'ics',
+          },
+        ],
+      });
+    } catch (err) {}
+  }
 }
